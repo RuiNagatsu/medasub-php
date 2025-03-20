952485d612c6e070fec500b27be0682386172527
RuiNagatsu <ruibonneiw@gmail.com>
2025-03-20 01:48:50 +0000

    sabado php

--- app/index.php
+++ app/index.php
@@ -1,8 +1,27 @@
 <?php
-// require './vendor/autoload.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-// use Application\DBConnection\MySQLConnection;
+use Slim\Views\PhpRenderer;
 
-// $db = new MySQLConnection();
+require __DIR__ . '/vendor/autoload.php';
 
-echo phpinfo();
+$app = AppFactory::create();
+
+$app->get('/', function(Request $request, Response $response, $args) {
+    $response->getBody()->write("<strong><em>Olá Porra!!!</em></strong>");
+    return $response;
+});
+
+$app->get('/mensagem/{nome}', function(Request $request, Response $response, $args) { 
+    $response->getBody()->write("<strong><em>Tchau Porra!!!</em></strong>");
+    return $response;
+});
+
+$app->get('/hello', function(Request $request, Response $response, $args){
+    $renderer = new PhpRenderer(__DIR__ . '/templates');
+    return $renderer->render($response, 'hello.php',['mensagem' => $msg] );
+});
+
+$app->run();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bb4c0ef400769c2efa402666de1ef6a6a6ebe41c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bb4c0ef400769c2efa402666de1ef6a6a6ebe41c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/http-message' => array(
+            'pretty_version' => '2.0',
+            'version' => '2.0.0.0',
+            'reference' => '402d35bcb92c70c026d1a6a9883f06b2ead23d71',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-message',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'slim/php-view' => array(
+            'pretty_version' => '3.4.0',
+            'version' => '3.4.0.0',
+            'reference' => 'ef1821663a6a028b9e446e8c6818fd257bf70313',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../slim/php-view',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
